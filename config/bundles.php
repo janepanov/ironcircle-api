0775985b1654ed9ee28977368779678d3dd3ad25
Jane Panov <panovjane@gmail.com>
2026-01-11 19:18:10 +0100

    Install core backend dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,8 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Somnambulist\Bundles\FractalBundle\SomnambulistFractalBundle::class => ['all' => true],
 ];
